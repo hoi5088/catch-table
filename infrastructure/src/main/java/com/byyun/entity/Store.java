4f9a9d0efdd1066fcadb534c63410c097dd148d9
윤복영 <phantom@jobkorea.co.kr>
2023-09-25 20:52:30 +0900

    StoreBusinessInfo, Store Entity 추가

--- infrastructure/src/main/java/com/byyun/entity/Store.java
+++ infrastructure/src/main/java/com/byyun/entity/Store.java
@@ -3,11 +3,14 @@ package com.byyun.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,4 +31,11 @@ public class Store {
 
 	@OneToMany(mappedBy = "store")
 	private List<Seat> seats = new ArrayList<>();
+
+	@OneToMany(mappedBy = "store")
+	private List<BusinessHour> businessHours = new ArrayList<>();
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="store_id",referencedColumnName = "store_id")
+	private StoreBusinessInfo storeBusinessInfo;
 }
